eb3717ff5f36e69fae4af38f2da34f71596e4bc1
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2022-04-15 13:10:23 +0600

    Add

--- Neurotuner/MainWindow.xaml.cs
+++ Neurotuner/MainWindow.xaml.cs
@@ -12,6 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CSCore;
+using CSCore.SoundIn;//Вход звука
+using CSCore.SoundOut;//Выход звука
+using CSCore.CoreAudioAPI;
+using CSCore.Streams;
+using CSCore.Codecs;
+using CSCore.Streams.Effects;
+using CSCore.DSP;
 
 namespace Neurotuner
 {
@@ -20,9 +28,42 @@ namespace Neurotuner
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MMDeviceCollection mInputDevices;
+        private MMDeviceCollection mOutputDevices;
+        private WasapiCapture mSoundIn;
+        private WasapiOut mSoundOut;
+        private SampleDSP mDsp, mDspR, mDsp1, mDspR1;
+        private SimpleMixer mMixer;
+        private ISampleSource mMp3;
+        private IWaveSource mSource;
+        private int SampleRate;
+        private int SampleRate1 = 48000;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Находит устройства для захвата звука и заполнияет комбобокс
+            MMDeviceEnumerator deviceEnum = new MMDeviceEnumerator();
+            mInputDevices = deviceEnum.EnumAudioEndpoints(DataFlow.Capture, DeviceState.Active);
+            MMDevice activeDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Multimedia);
+            foreach (MMDevice device in mInputDevices)
+            {
+                cmbInput.Items.Add(device.FriendlyName);
+                if (device.DeviceID == activeDevice.DeviceID) cmbInput.SelectedIndex = cmbInput.Items.Count - 1;
+            }
+
+            //Находит устройства для вывода звука и заполняет комбобокс
+            activeDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            mOutputDevices = deviceEnum.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active);
+            foreach (MMDevice device in mOutputDevices)
+            {
+                cmbOutput.Items.Add(device.FriendlyName);
+                if (device.DeviceID == activeDevice.DeviceID) cmbOutput.SelectedIndex = cmbOutput.Items.Count - 1;
+            }
         }
     }
 }
